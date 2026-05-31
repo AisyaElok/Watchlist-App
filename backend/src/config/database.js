c7bafd89d7f2f5c76d599f35a9db1b9fc5c2ae0a
xe3u99 <isyael90@gmail.com>
2026-05-31 23:19:50 +0700

    fix firebase config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin')
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require('../../serviceAccountKey.json')
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://watchlistapp-lokiie-default-rtdb.asia-southeast1.firebasedatabase.app',
-    httpAgent: new (require('https').Agent)({ keepAlive: false })
+    databaseURL: 'https://watchlistapp-lokiie-default-rtdb.asia-southeast1.firebasedatabase.app'
   })
 }
 
